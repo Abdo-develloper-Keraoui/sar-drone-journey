f552737a11dca1bfe3f30104b7bc7b1a4a430409
Abdo-develloper-Keraoui <abdottawabkeraoui@gmail.com>
2026-04-29 20:34:24 +0100

    Add source files and update .gitignore to ignore compiled outputs binaries specific for the linux os and they don't have a .o extension

---
+++ month-01/week-01/ex1-8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+    //ex1: breaking down the code to see what kinds of errors do i get! 
+    print(Hello, World!\n") for example
+    */
+
+    /*
+    //ex2:
+    printf("Hello, World!\c"); ----> this prints the c char after the \ char, so it is not a special char at all 
+    */
+
+    
+
+    
+}
